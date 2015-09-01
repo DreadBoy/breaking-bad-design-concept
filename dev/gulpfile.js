e3b834f97265ea7dbc4a00238024a474613affeb
Matic Leva <vertical3life@gmail.com>
2015-09-01 16:00:27 +0200

    Index site created

---
+++ dev/gulpfile.js
@@ -0,0 +1,32 @@
+'use strict';
+
+var gulp = require('gulp'),
+  sass = require('gulp-sass'),
+  minify = require('gulp-minify'),
+  minifyCss = require('gulp-minify-css'),
+  sourcemaps = require('gulp-sourcemaps');
+
+gulp.task('sass', function() {
+  return gulp.src('./css/style.scss')
+    .pipe(sass().on('error', sass.logError))
+    .pipe(gulp.dest('./css'));
+});
+
+gulp.task('sass:watch', ['sass'], function() {
+  gulp.watch('./css/*.scss', ['sass']);
+});
+
+gulp.task('dev', ['sass:watch'])
+
+gulp.task('default', ['dev'])
+
+gulp.task('build-sass', function() {
+  return gulp.src('./css/style.scss')
+    .pipe(sass().on('error', sass.logError))
+    .pipe(sourcemaps.init())
+    .pipe(minifyCss())
+    .pipe(sourcemaps.write())
+    .pipe(gulp.dest('./../dist/css'));
+});
+
+gulp.task('build', ['build-sass'])
